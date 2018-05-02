56a215c22533c139adf3611d6976cb420fc58b6b
Shiqi Chou <shiqic@andrew.cmu.edu>
2018-05-01 20:16:45 -0400

    models created

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ApplicationRecord
+    belong_to :category
+
+    validates_presence_of :title, :content
+
+    scope :alphabetical, -> { order('title')}
+    scope :active, -> { where('active = ?', true)}
+end
